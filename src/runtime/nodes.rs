536e51867fd6977e7b001b97cf592014e5477642
Joey de Ruiter <contact@joeyderuiter.me>
2020-12-05 23:22:05 +0100

    More refactor stuff

---
+++ src/runtime/nodes.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+
+use crate::ast::nodes::{Literal, VariableLiteral};
+
+pub struct FunctionCall {
+    pub function_type: FunctionCallType,
+    pub arguments: Vec<Literal>,
+}
+
+pub struct SystemCall {
+    pub func: Box<dyn Fn(&HashMap<String, Literal>) -> Option<Literal>>,
+}
+
+impl SystemCall {
+    fn new(func: impl Fn(&HashMap<String, Literal>) -> Option<Literal> + 'static) -> Self {
+        Self { 
+            func: Box::new(func)
+        }
+    }
+}
+
+pub enum FunctionCallType {
+    SystemCall(SystemCall),
+    RuntimeCall
+}
+
+pub struct BlockScope {
+    pub functions: HashMap<String, FunctionCall>,
+    pub variables: HashMap<String, VariableLiteral>,
+}
+
+impl BlockScope {
+    pub fn new() -> BlockScope {
+        BlockScope {
+            functions: HashMap::<String, FunctionCall>::new(),
+            variables: HashMap::<String, VariableLiteral>::new(),
+        }
+    }
+
+    pub fn new_root() -> BlockScope {
+        let mut functions = HashMap::<String, FunctionCall>::new();
+
+        functions.insert("log".to_string(), FunctionCall { 
+            function_type: FunctionCallType::SystemCall(
+                SystemCall::new(|x| { 
+                    let output = x.get(&"output".to_string()).unwrap(); 
+
+                    println!("{}", output.value);
+                    
+                    None
+                })), 
+                arguments: vec![Literal::from_str("output")]
+            }
+        );
+
+        BlockScope {
+            functions,
+            variables: HashMap::<String, VariableLiteral>::new(),
+        }
+    }
+}
---
+++ src/runtime/parsers/expression_statement.rs
@@ -0,0 +1,91 @@
+use std::collections::HashMap;
+
+use crate::{ast::nodes::ExpressionStatement, util::is_identifier, ast::nodes::{CallExpressionCallee, ExpressionStatementExpression, Literal, VariableLiteral}, runtime::{nodes::FunctionCallType, Runtime}, util::is_number};
+
+pub fn parse_expression_statement(runtime: &mut Runtime, statement: &ExpressionStatement) {
+    let name = parse_expression_statement_name(&statement);
+    runtime.new_scope();
+    let function = runtime.function(&name);
+
+    if function.is_none() {
+        runtime.pop_scope();
+        return;
+    }
+
+    // Create temporary scope
+    let function = function.unwrap();
+
+    let args_values = parse_expression_statement_args(statement);
+    let args = &function.arguments;
+    
+    if args.len() != args_values.len() {
+        panic!("Amount of params do not match");
+    }
+
+    let mut args_map = HashMap::<String, Literal>::new();
+
+    for index in 0..args.len() {
+        let key = args.get(index).unwrap().value.clone();
+        let mut value = args_values.get(index)
+            .unwrap()
+            .clone();
+
+        // Fetch actual value if it is an variable
+        let first_char = value.raw_value.chars()
+            .nth(0)
+            .unwrap();
+
+        if is_identifier(&first_char) {
+            let variable_name = value.value.clone();
+            let variable = runtime.variable(&variable_name);
+
+            if variable.is_none() {
+                panic!("{:?} is not defined", variable)
+            }
+
+
+            match variable.unwrap() {
+                VariableLiteral::Literal(literal) => {
+                    value = literal.clone();
+                },
+                _ => { println!("Unsupported variable type") }
+            }
+
+        }
+
+        args_map.insert(key, value);
+    }
+
+    match &function.function_type {
+        FunctionCallType::SystemCall(syscall) => {
+            (syscall.func)(&args_map);
+        },
+        _ => { panic!("Unsupported function type") }
+    }
+
+    // Exit temporary scope
+    runtime.pop_scope();
+}
+
+fn parse_expression_statement_args(statement: &ExpressionStatement) -> &Vec<Literal> {
+    match &statement.expression {
+        ExpressionStatementExpression::CallExpression(expression) => {
+            &expression.arguments
+        },
+        _ => { panic!("Unsupported expression: {:?}", &statement.expression) }
+    }
+}
+
+fn parse_expression_statement_name(statement: &ExpressionStatement) -> String {
+    match &statement.expression {
+        ExpressionStatementExpression::CallExpression(expression) => {
+            match &expression.callee {
+                CallExpressionCallee::Identifier(identifier) => {
+                    identifier.name.to_string()
+                }
+                _ => { panic!("Unsupported expression callee: {:?}", &expression.callee) }
+            }
+        },
+        _ => { panic!("Unsupported expression: {:?}", &statement.expression) }
+    }
+}
